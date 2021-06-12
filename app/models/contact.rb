0b9f43d94c8db7554703f683a73cd9db6029ead2
Beryl Armand <beryl.armand@gmail.com>
2021-06-12 17:23:14 +0200

    changed email account

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -6,7 +6,7 @@ class Contact < MailForm::Base
     {
       #this is the subject for the email generated, it can be anything you want
       subject: "My Contact Form",
-      to: 'beryl.armand@gmail.com',
+      to: 'beryl.koller.dev@gmail.com',
       from: %("#{name}" <#{email}>)
       #the from will display the name entered by the user followed by the email
     }
