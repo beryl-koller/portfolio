8292023e5cc5c14203f845a06d0e241b0413a6a1
Beryl Armand <beryl.armand@gmail.com>
2021-06-12 12:38:39 +0200

    nav moving

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -7,7 +7,7 @@ class ContactsController < ApplicationController
     @contact = Contact.new(params[:contact])
     @contact.request = request
     if @contact.deliver
-      redirect_to root_path, notice: 'Message sent successfully'
+      redirect_to root_path, notice: 'Merci, votre message a bien été envoyé. Je reviendrai vers vous très rapidement.'
     else
       flash.now[:error] = 'Cannot send message'
       render :new
