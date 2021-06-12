9fc58d89c8aef55df1f50f559948dd7dad598c32
Beryl Armand <beryl.armand@gmail.com>
2021-06-12 13:38:59 +0200

    try fix for prod

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,12 +37,12 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors= true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address:              'smtp.gmail.com',
+    address:              "smtp.gmail.com",
     port:                 587,
-    domain:               'gmail.com',
+    domain:               "gmail.com",
     user_name:            ENV["GMAIL_USERNAME"],
     password:             ENV["GMAIL_PASSWORD"],
-    authentication:       'plain',
+    authentication:       "plain",
   }
   config.action_mailer.perform_caching = false
 
